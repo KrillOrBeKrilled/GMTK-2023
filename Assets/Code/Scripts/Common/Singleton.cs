674444cb11a8bdf01dfde31ae64b5da50d23beeb
eon704 <eon704@gmail.com>
2023-07-06 21:12:29 +0600

    Empty project

---
+++ Assets/Code/Scripts/Common/Singleton.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component {
+  public static T Instance { get; private set; }
+
+  protected virtual void Awake() {
+    // Delete this object if another instance already exists.
+    if (Instance != null && Instance != this as T) {
+      Destroy(this);
+    } else {
+      Instance = this as T;
+    }
+  }
+}

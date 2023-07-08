27b62f81bd57d9f0f41f9f1376a11b84cf66008f
eon704 <eon704@gmail.com>
2023-07-08 13:21:25 +0600

    Setup test scene

--- Assets/Code/Scripts/Traps/TestTrap1.cs
+++ Assets/Code/Scripts/Traps/TestTrap1.cs
@@ -1,19 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace Traps
 {
     public class TestTrap1 : Trap
     {
         public override void AdjustSpawnPoint()
         {
-            
+
         }
 
         public override void Detonate()
         {
-            
+            print("Detonating!");
         }
     }
 }
--- Assets/Code/Scripts/Traps/Trap.cs
+++ Assets/Code/Scripts/Traps/Trap.cs
@@ -11,5 +11,14 @@ namespace Traps
 
         public abstract void AdjustSpawnPoint();
         public abstract void Detonate();
+
+        private void OnTriggerEnter2D(Collider2D other) {
+            print("Triggered!");
+            if (!other.CompareTag("Player"))
+                return;
+
+            print("Triggered by player!");
+            this.Detonate();
+        }
     }
 }

ef672aec26b020d4a4f6e9828b5860a73a2148ce
Shaelynn <80Currans@gmail.com>
2023-07-08 04:50:01 -0400

    Completed trap placement seeking and validation
    Completed the implementation of the trap placement seeking (coloring the tiles) and the validation to know when a trap can or cannot be placed

--- Assets/Code/Scripts/Traps/Trap.cs
+++ Assets/Code/Scripts/Traps/Trap.cs
@@ -7,8 +7,24 @@ namespace Traps
     // Parent trap class
     public abstract class Trap : MonoBehaviour
     {
-        [SerializeField] protected List<Transform> _gridPoints;
+        [SerializeField] protected List<Vector3Int> _leftGridPoints, _rightGridPoints;
+        [SerializeField] protected int _validationScore;
 
+        public List<Vector3Int> GetLeftGridPoints()
+        {
+            return _leftGridPoints;
+        }
+        
+        public List<Vector3Int> GetRightGridPoints()
+        {
+            return _rightGridPoints;
+        }
+        
+        public bool IsValidScore(int score)
+        {
+            return score >= _validationScore;
+        }
+        
         public abstract void AdjustSpawnPoint();
         public abstract void Detonate();
     }

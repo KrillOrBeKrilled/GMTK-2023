334f2343ae2604daf0d1d36142cabed592eb9094
eon704 <eon704@gmail.com>
2023-07-28 00:20:22 +0700

    Added exposed events for buttons and pause events

---
+++ Assets/Code/Scripts/UI/ButtonExtension.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ButtonExtension : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler {
+  [SerializeField] private UnityEvent _onClick;
+  [SerializeField] private UnityEvent _onHover;
+
+  public void OnPointerEnter(PointerEventData eventData) {
+    this._onHover?.Invoke();
+  }
+
+  // The OnClicked event of the UI.Button component can also be used directly
+  public void OnPointerClick(PointerEventData eventData) {
+    this._onClick?.Invoke();
+  }
+}

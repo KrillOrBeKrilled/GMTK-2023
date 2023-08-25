b7e88f6cbb88aa55321d7477d06baeeca940fc41
Shaelynn <80Currans@gmail.com>
2023-08-25 13:28:16 -0400

    Adjusted acid trap logic to new mesh
    Updated the acid trap script to work correctly now that the spriterenderer has been replaced with a mesh

--- Assets/Code/Scripts/Traps/AcidPitTrap.cs
+++ Assets/Code/Scripts/Traps/AcidPitTrap.cs
@@ -4,40 +4,40 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class AcidPitTrap : Trap {
+  [SerializeField] private Material _acidLiquid;
+  [SerializeField] private Material _heatEmanating;
+  [SerializeField] private GameObject _bubbles;
   [SerializeField] private int _damageAmount;
   [SerializeField] private GameObject _pitAvoidanceJumpPad;
-
+  
   private readonly WaitForSeconds _waitForOneSecond = new WaitForSeconds(1f);
   private Coroutine _intervalDamageCoroutine;
 
-  // public override void Construct(Vector3 spawnPosition, Canvas canvas, 
-  //   Vector3Int[] tilePositions, PlayerSoundsController soundsController)
-  // {
-  //   // Initialize all the bookkeeping structures we will need
-  //   SpawnPosition = spawnPosition;
-  //   TilePositions = tilePositions;
-  //   _soundsController = soundsController;
-  //           
-  //   // Delete/invalidate all the tiles overlapping the trap
-  //   TilemapManager.Instance.ClearLevelTiles(TilePositions);
-  //
-  //   // Spawn a slider to indicate the progress on the build
-  //   GameObject sliderObject = Instantiate(SliderBar, canvas.transform);
-  //   sliderObject.transform.position = spawnPosition + AnimationOffset + Vector3.up;
-  //   _buildCompletionBar = sliderObject.GetComponent<Slider>();
-  //
-  //   // Trap deployment visuals
-  //   transform.position = spawnPosition + Vector3.up * 3f;
-  //   transform.DOMove(spawnPosition + Vector3.up * AnimationOffset.y, 0.2f);
-  //
-  //   var sprite = GetComponent<SpriteRenderer>();
-  //   var color = sprite.color;
-  //   sprite.color = new Color(color.r, color.g, color.b, 0);
-  //   sprite.DOFade(1, 0.4f);
-  //
-  //   // Initiate the build time countdown
-  //   ConstructionCompletion = 0;
-  // }
+  public override void Construct(Vector3 spawnPosition, Canvas canvas, 
+    Vector3Int[] tilePositions, PlayerSoundsController soundsController)
+  {
+    // Initialize all the bookkeeping structures we will need
+    SpawnPosition = spawnPosition;
+    TilePositions = tilePositions;
+    SoundsController = soundsController;
+            
+    // Delete/invalidate all the tiles overlapping the trap
+    TilemapManager.Instance.ClearLevelTiles(TilePositions);
+  
+    // Spawn a slider to indicate the progress on the build
+    GameObject sliderObject = Instantiate(SliderBar, canvas.transform);
+    sliderObject.transform.position = spawnPosition + AnimationOffset + Vector3.up;
+    BuildCompletionBar = sliderObject.GetComponent<Slider>();
+  
+    // Trap deployment visuals
+    transform.position = spawnPosition;
+    _acidLiquid.SetFloat("_Depth", 0);
+    // _heatEmanating.SetFloat("_HazeRange", 0);
+    _bubbles.SetActive(false);
+
+    // Initiate the build time countdown
+    ConstructionCompletion = 0;
+  }
   
   public override Vector3 GetLeftSpawnPoint(Vector3 origin)
   {

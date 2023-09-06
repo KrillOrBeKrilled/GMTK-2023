f6ee3364cc948f5295daa3c3e3021bdf5b8431ec
eon704 <eon704@gmail.com>
2023-09-06 17:54:07 -0400

    Dialogue-GameManager system reworked

--- Assets/Code/Scripts/Managers/GameManager.cs
+++ Assets/Code/Scripts/Managers/GameManager.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UGSAnalytics;
 using UI;
-using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.Events;
 using Yarn.Unity;
@@ -38,17 +37,15 @@ namespace Managers {
     public UnityEvent<string> OnHenLost { get; private set; }
     public UnityEvent<Hero> OnHeroSpawned { get; private set; }
 
-    // private Hero _hero;
-    private List<Hero> _heroes = new List<Hero>();
+    private Hero _heroActor;
+    private readonly List<Hero> _heroes = new List<Hero>();
 
     private EndgameTarget _endgameTarget;
-    private List<RespawnPoint> _respawnPoints = new List<RespawnPoint>();
+    private readonly List<RespawnPoint> _respawnPoints = new List<RespawnPoint>();
     private RespawnPoint _firstRespawnPoint;
     private RespawnPoint _activeRespawnPoint;
 
     private LevelData _levelData;
-
-    // TODO : Fix dialogue
 
     public void LoadMainMenu() {
       PauseManager.Instance.UnpauseGame();
@@ -76,6 +73,8 @@ namespace Managers {
 
       // Create a copy to avoid modifying data source
       this._levelData = ScriptableObject.CreateInstance<LevelData>();
+      this._levelData.Type = this._levelDataFile.Type;
+      this._levelData.DialogueName = this._levelDataFile.DialogueName;
       this._levelData.EndgameTargetPosition = this._levelDataFile.EndgameTargetPosition;
       this._levelData.RespawnPositions = this._levelDataFile.RespawnPositions.ToList();
       this._levelData.WavesData = new WavesData() { WavesList = this._levelDataFile.WavesData.WavesList.ToList() };
@@ -91,55 +90,83 @@ namespace Managers {
       this._firstRespawnPoint = this._activeRespawnPoint;
     }
 
-    [YarnCommand("enter_hero")]
+#region YarnCommands
+
+    [YarnCommand("spawn_hero_actor")]
+    public void SpawnHeroActor() {
+      this._heroActor = this.SpawnHero();
+
+      if (this._heroActor.TryGetComponent(out YarnCharacter newYarnCharacter)) {
+        YarnCharacterView.instance.RegisterYarnCharacter(newYarnCharacter);
+        YarnCharacterView.instance.playerCharacter = newYarnCharacter;
+      }
+    }
+
+    [YarnCommand("enter_hero_actor")]
     public void EnterHero() {
-      // this._hero.EnterLevel();
+      this._heroActor.EnterLevel();
     }
 
     [YarnCommand("start_level")]
     public void StartLevel() {
-      // For playtesting analytics, start recording the player input for the session
+      // Start recording the player input for the session
       this._playerManager.PlayerController.StartSession();
+      this._heroActor?.StartRunning();
 
-      // this._hero.StartRunning();
       CoinManager.Instance.StartCoinEarning();
       this.OnStartLevel?.Invoke();
     }
+
+#endregion
 
     public void SkipDialogue() {
       if (this._dialogueRunner.IsDialogueRunning) {
         this._dialogueRunner.Stop();
       }
-
+      
       this._cameraShaker.StopShake();
       this._cameraSwitcher.ShowPlayer();
       this.StartLevel();
     }
 
     private void Start() {
-      // Setup
       this._gameUI.Initialize(this, this._playerManager);
 
       this._endgameTarget.OnHeroReachedEndgameTarget.AddListener(this.HeroReachedLevelEnd);
       this._playerManager.PlayerController.OnPlayerStateChanged.AddListener(this.OnPlayerStateChanged);
       this._playerManager.PlayerController.OnSelectedTrapIndexChanged.AddListener(this.SelectedTrapIndexChanged);
       this._playerManager.PlayerController.OnTrapDeployed.AddListener(this.OnTrapDeployed);
-      this._playerManager.Initialize(this._firstRespawnPoint.transform, this._endgameTarget.transform);
 
-      this.StartCoroutine(this.SpawnNextWave());
-      // this._hero.HeroMovement.ToggleMoving(false);
+      this._playerManager.Initialize(this._firstRespawnPoint.transform, this._endgameTarget.transform);
 
       this.OnSetupComplete?.Invoke();
 
-      if (PlayerPrefsManager.ShouldSkipDialogue()) {
-        this.SkipDialogue();
+      if (this._levelData.Type == LevelData.LevelType.Story) {
+        this.StartStoryLevel();
       } else {
-        this._cameraSwitcher.ShowStart();
-        this._cameraShaker.StopShake();
-        this._dialogueRunner.StartDialogue(this._dialogueRunner.startNode);
+        this.StartEndlessLevel();
       }
 
       PauseManager.Instance.SetIsPausable(true);
+    }
+
+    private void StartStoryLevel() {
+      if (PlayerPrefsManager.ShouldSkipDialogue()) {
+        this.SkipDialogue();
+        return;
+      }
+
+      if (!this._dialogueRunner.yarnProject.NodeNames.Contains(this._levelData.DialogueName)) {
+        Debug.LogError("Missing or Incorrect Dialogue Name, make sure provided dialogue name value is correct");
+        return;
+      }
+
+      this._dialogueRunner.StartDialogue(this._levelData.DialogueName);
+    }
+
+    private void StartEndlessLevel() {
+      this.SkipDialogue();
+      this.StartCoroutine(this.SpawnNextWave());
     }
 
     private void OnPlayerStateChanged(IPlayerState state, float xPos, float yPos, float zPos) {
@@ -147,24 +174,23 @@ namespace Managers {
 
       this.HenDied("The Hero managed to take you down Hendall.\nDon't you dream about that promotion I mentioned last time!");
 
-      // Send Analytics data
       if (UGS_Analytics.Instance is null) return;
-
       UGS_Analytics.PlayerDeathByHeroCustomEvent(CoinManager.Instance.Coins, xPos, yPos, zPos);
     }
 
     private void SelectedTrapIndexChanged(int trapIndex) {
-      var isAffordable = this._playerManager.PlayerController.GetTrapCost() >= CoinManager.Instance.Coins;
+      if (UGS_Analytics.Instance is null) {
+        return;
+      }
 
-      // Send Analytics data
-      if (UGS_Analytics.Instance is null) return;
-
+      bool isAffordable = this._playerManager.PlayerController.GetTrapCost() >= CoinManager.Instance.Coins;
       UGS_Analytics.SwitchTrapCustomEvent(trapIndex, isAffordable);
     }
 
     private void OnTrapDeployed(int trapIndex) {
-      // Send Analytics data
-      if (UGS_Analytics.Instance is null) return;
+      if (UGS_Analytics.Instance is null) {
+        return;
+      }
 
       UGS_Analytics.DeployTrapCustomEvent(trapIndex);
     }
@@ -180,34 +206,30 @@ namespace Managers {
       bool noMoreWaves = this._levelData.WavesData.WavesList.Count <= 0;
       bool allHeroesDied = this._heroes.Count <= 0;
       if (noMoreWaves && allHeroesDied) {
-        // TODO: better text line later?
         this.OnHenWon?.Invoke("All heroes were defeated. Good job!");
       } else if (allHeroesDied) {
         this.StartCoroutine(this.SpawnNextWave());
       }
 
-      // Send Analytics data
       if (UGS_Analytics.Instance is null) return;
       Vector3 heroPos = hero.transform.position;
       UGS_Analytics.HeroDiedCustomEvent(heroPos.x, heroPos.y, heroPos.z);
     }
 
     private void OnHeroIsStuck(float xPos, float yPos, float zPos) {
-      // Send Analytics data
       if (UGS_Analytics.Instance is null) return;
-
       UGS_Analytics.HeroIsStuckCustomEvent(xPos, yPos, zPos);
     }
 
     private void HeroReachedLevelEnd() {
       this._playerManager.PlayerController.DisablePlayerInput();
-      // this._hero.HeroMovement.ToggleMoving(false);
+      this.StopAllHeroes();
       this.OnHenLost?.Invoke("The Hero managed to reach his goal and do heroic things.\nHendall, you failed me!");
     }
 
     private void HenDied(string message) {
       this._playerManager.PlayerController.DisablePlayerInput();
-      // this._hero.HeroMovement.ToggleMoving(false);
+      this.StopAllHeroes();
 
       Destroy(this._playerManager.gameObject);
       this.OnHenLost?.Invoke(message);
@@ -235,19 +257,21 @@ namespace Managers {
       yield return this.SpawnNextWave();
     }
 
-    private void SpawnHero() {
+    private Hero SpawnHero() {
       Hero newHero = Instantiate(this._heroPrefab, this._activeRespawnPoint.transform);
       newHero.Initialize(this._firstRespawnPoint.transform, this._endgameTarget.transform);
       newHero.OnHeroDied.AddListener(this.OnHeroDied);
       newHero.HeroMovement.OnHeroIsStuck.AddListener(this.OnHeroIsStuck);
 
-      if (newHero.TryGetComponent(out YarnCharacter newYarnCharacter)) {
-        YarnCharacterView.instance.RegisterYarnCharacter(newYarnCharacter);
-        YarnCharacterView.instance.playerCharacter = newYarnCharacter;
-      }
-
       this._heroes.Add(newHero);
       this.OnHeroSpawned.Invoke(newHero);
+      return newHero;
+    }
+
+    private void StopAllHeroes() {
+      foreach (Hero hero in this._heroes) {
+        hero.HeroMovement.ToggleMoving(false);
+      }
     }
   }
 }
--- Assets/Code/Scripts/Model/LevelData.cs
+++ Assets/Code/Scripts/Model/LevelData.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 namespace Model {
   [CreateAssetMenu(fileName = "LevelData", menuName = "LevelData")]
   public class LevelData : ScriptableObject {
-    public Vector3 EndgameTargetPosition;
+    public LevelType Type = LevelType.Endless;
+    public string DialogueName;
+    public Vector3 EndgameTargetPosition = Vector3.zero;
     public List<Vector3> RespawnPositions;
     public WavesData WavesData;
+
+    public enum LevelType {
+      Story,
+      Endless
+    }
   }
 }

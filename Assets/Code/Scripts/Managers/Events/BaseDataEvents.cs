c65af8cc78a01729f5433463721e9fe3a843417a
Shaelynn <80Currans@gmail.com>
2023-11-30 19:00:46 -0500

    Updated and organized Managers assembly documentation
    Updated and organized the existing documentation of the Managers assembly according to the new features for consistency

--- Assets/Code/Scripts/Managers/Events/BaseDataEvents.cs
+++ Assets/Code/Scripts/Managers/Events/BaseDataEvents.cs
@@ -1,6 +1,20 @@
 using UnityEngine.Events;
 
 namespace KrillOrBeKrilled.Managers {
-  public class CoinAmountChangedEvent : UnityEvent<int> {}
-  public class PauseToggledEvent : UnityEvent<bool> {}
+    //*******************************************************************************************
+    // CoinAmountChangedEvent
+    //*******************************************************************************************
+    /// <summary>
+    /// A callback that tracks when the coin quantity of the coin management system has
+    /// been updated.
+    /// </summary>
+    public class CoinAmountChangedEvent : UnityEvent<int> {}
+    
+    //*******************************************************************************************
+    // PauseToggledEvent
+    //*******************************************************************************************
+    /// <summary>
+    /// A callback that tracks when the game should be paused or unpaused.
+    /// </summary>
+    public class PauseToggledEvent : UnityEvent<bool> {}
 }

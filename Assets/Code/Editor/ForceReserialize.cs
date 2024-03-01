b2a9c6ad549932766d4470c466774fc9d5500103
Arman Akhmetov <eon704@gmail.com>
2024-03-01 04:58:50 -0500

    Reserialization Utility

--- Assets/Code/Editor/ForceReserialize.cs
+++ Assets/Code/Editor/ForceReserialize.cs
@@ -5,21 +5,37 @@ using UnityEngine;
 // ForceReserialize
 //*******************************************************************************************
 namespace KrillOrBeKrilled.Editor {
-    public class ForceReserialize : EditorWindow {
-        [MenuItem("Window/ForceReserialize")]
-        public static void ShowWindow() {
-            GetWindow(typeof(ForceReserialize));
-        }
-
-        public void OnGUI() {
-            if (GUILayout.Button("Force reserialize all assets.")) {
-                PerformForceReserialize();
+    public static class ReserializeAllAssets {
+        [MenuItem("Henchman/Force Reserialize All Assets")]
+        public static void ForceReserializeAllAssets() {
+            if (EditorUtility.DisplayDialog("Force Reserialize All Assets",
+                                            "This will re-serialize all assets and might take some time. Proceed?",
+                                            "Yes", "No")) {
+                string[] allAssetPaths = AssetDatabase.GetAllAssetPaths();
+                
+                Debug.Log("Starting re-serialization of all assets...");
+                AssetDatabase.ForceReserializeAssets();
+                Debug.Log($"Re-serialization complete. {allAssetPaths.Length} assets re-serialized.");
             }
         }
+    }
+    
+    public static class ForceReserializeAssetsUtility {
+        [MenuItem("Henchman/Force Reserialize Selected", true)]
+        private static bool ForceReserializeAssetsValidation() {
+            return Selection.assetGUIDs.Length > 0;
+        }
 
-        private static void PerformForceReserialize() {
-            AssetDatabase.ForceReserializeAssets();
-            Debug.Log("Completed");
+        [MenuItem("Henchman/Force Reserialize Selected")]
+        private static void ForceReserializeSelectedAssets() {
+            string[] selectedAssetPaths = Selection.assetGUIDs;
+
+            for (int i = 0; i < selectedAssetPaths.Length; i++) {
+                selectedAssetPaths[i] = AssetDatabase.GUIDToAssetPath(selectedAssetPaths[i]);
+            }
+
+            AssetDatabase.ForceReserializeAssets(selectedAssetPaths);
+            Debug.Log($"Forced re-serialization on {selectedAssetPaths.Length} assets.");
         }
     }
 }

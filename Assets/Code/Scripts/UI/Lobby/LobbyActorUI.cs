fe4881ebd78d569fe8b0f49fe9bc1b86a098eb80
Arman Akhmetov <eon704@gmail.com>
2024-05-22 03:43:20 -0400

    Base Movement around scene

---
+++ Assets/Code/Scripts/UI/Lobby/LobbyActorUI.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using KrillOrBeKrilled.Extensions;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace KrillOrBeKrilled.UI {
+  public class LobbyActorUI : MonoBehaviour {
+    [SerializeField] private float _moveDelay = 5f;
+    [FormerlySerializedAs("_actorImageRectTransform")] [SerializeField] private RectTransform _actorRectTransform;
+    [SerializeField] private Animator _actorAnimator;
+    [SerializeField] private List<RectTransform> _targets;
+
+    private static readonly int IdleId = Animator.StringToHash("Idle");
+    private static readonly int WalkId = Animator.StringToHash("Walk");
+
+    private bool _doRoam = true;
+    private bool _isMoving;
+    private RectTransform _oldPos;
+    
+    private void Start() {
+      this.StartCoroutine(this.Roam());
+    }
+
+
+    private IEnumerator Roam() {
+      while (this._doRoam) {
+        this.StartIdle();
+        yield return new WaitForSeconds(this._moveDelay);
+        
+        this.StartWalk();
+        this.MoveActor();
+        yield return new WaitWhile(() => this._isMoving);
+      }
+    }
+
+
+    private void MoveActor() {
+      this._isMoving = true;
+      RectTransform target;
+      do {
+        target = this._targets.GetRandomElement();
+      } while (target == this._oldPos);
+
+      this._oldPos = target;
+      this._actorRectTransform
+          .DOAnchorPos(target.anchoredPosition, 3f)
+          .SetEase(Ease.Linear)
+          .OnComplete(() => this._isMoving = false);
+    }
+    
+
+    private void StartIdle() {
+      this._actorAnimator.ResetTrigger(LobbyActorUI.WalkId);
+      this._actorAnimator.SetTrigger(LobbyActorUI.IdleId);
+    }
+    
+    
+    private void StartWalk() {
+      this._actorAnimator.SetTrigger(LobbyActorUI.WalkId);
+      this._actorAnimator.ResetTrigger(LobbyActorUI.IdleId);
+    }
+  }
+}

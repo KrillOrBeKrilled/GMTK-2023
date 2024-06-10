5dd2fcacde9219b2cc0780e105cfd4ee1cb3f3cc
Arman Akhmetov <eon704@gmail.com>
2024-06-10 04:29:13 -0400

    Resources dim in Acid

--- Assets/Code/Scripts/Traps/Resources/ResourcePickup.cs
+++ Assets/Code/Scripts/Traps/Resources/ResourcePickup.cs
@@ -11,6 +11,7 @@ namespace KrillOrBeKrilled.Traps {
     public class ResourcePickup : MonoBehaviour {
         [SerializeField] private ResourcePickupData data;
         private Rigidbody2D _rigidbody2D;
+        private SpriteRenderer _spriteRenderer;
         
         // Currently, using a static event to keep Managers depending on Traps. (To be refactored later)
         public static event Action<ResourceType, int> OnResourceCollected;
@@ -22,13 +23,28 @@ namespace KrillOrBeKrilled.Traps {
         #region Unity Methods
 
         private void Awake() {
-            _rigidbody2D = GetComponent<Rigidbody2D>();
+            this._rigidbody2D = this.GetComponent<Rigidbody2D>();
+            this._spriteRenderer = this.GetComponent<SpriteRenderer>();
         }
 
         private void OnCollisionEnter2D(Collision2D other) {
             if (other.gameObject.CompareTag("Player")) {
                 OnResourceCollected?.Invoke(data.resourceType, data.quantity);
                 Destroy(gameObject);
+            }
+        }
+
+        private void OnTriggerStay2D(Collider2D other) {
+            AcidPitTrap trap = other.gameObject.GetComponent<AcidPitTrap>();
+            if (trap != null) {
+                this._spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+            }
+        }
+        
+        private void OnTriggerExit2D(Collider2D other) {
+            AcidPitTrap trap = other.gameObject.GetComponent<AcidPitTrap>();
+            if (trap != null) {
+                this._spriteRenderer.color = Color.white;
             }
         }
 
